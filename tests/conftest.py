2e526b27304511c58c4bbe4af971e88dceeba500
nicolas tchenio <nicolas.tchenio@gmail.com>
2026-03-19 11:58:58 +0400

    mise a jour lint 2

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,23 @@
+import os
 import pathlib
 import sys
-import os
 
 # Ajouter la racine du projet au sys.path
 sys.path.append(str(pathlib.Path(__file__).resolve().parent.parent))
 
-# On définit une variable d'environnement pour forcer l'usage de SQLite pendant les tests
-# On le fait AVANT d'importer app_api.modules.connect
+# Forcer SQLite pendant les tests AVANT d'importer app_api.modules.connect
 os.environ["DATABASE_URL"] = "sqlite:///./tests/test_db.sqlite"
 
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from app_api.modules.connect import Base
 
 # On recrée un engine de test pour SQLite
-engine = create_engine("sqlite:///./tests/test_db.sqlite", connect_args={"check_same_thread": False})
+engine = create_engine(
+    "sqlite:///./tests/test_db.sqlite", connect_args={"check_same_thread": False}
+)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 @pytest.fixture(scope="session", autouse=True)
@@ -34,9 +36,9 @@ def db_session():
     connection = engine.connect()
     transaction = connection.begin()
     session = TestingSessionLocal(bind=connection)
-    
+
     yield session
-    
+
     session.close()
     transaction.rollback()
     connection.close()
